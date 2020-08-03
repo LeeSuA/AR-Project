32390355db7f94d2cdff2bde547dacefbd58fc2b
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2020-08-04 04:00:41 +0900

    대충 지금까지 만든거 커밋
    1. 대충 리소스 수정
    2. 대충 훈련 저장기능
    3. 파이어베이스 패키지 일단 임포트
    
    <해야할것>
    + 파베 연동, 건물들 > 훈련들 > 훈련세부정보 에 관한 데이터 다루기
    + 초기 화면 Scene 만들기 : 건물선택(로그인) - 훈련생성/훈련참여
    + 훈련참여 Scene 만들기 : QR인식, 저장된 분기점 따라가서 일정 위치 도달시 종료. (QR은 한번만 쓰자!)
    + 소화기 사용해보는거 만들기 (막 핀뽑고 누르고 그거그거)
    * AR카메라 position 잘 초기화 된건지 디버그 한번 해보기

---
+++ EXITraining/Assets/Scripts/MakeDrillManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using TMPro;
+
+public class MakeDrillManager : MonoBehaviour
+{
+
+    public ARTrackedImageManager trackedImageManager;
+    public GameObject arSession_Origin;
+    public GameObject arSession;
+    public GameObject arCam;
+    public GameObject markerPrefab;
+    public GameObject cntText;
+
+    public GameObject makeButton, removeButton;
+
+    private List<GameObject> markerObjects = new List<GameObject>();
+    private List<Vector3> markersPosition = new List<Vector3>();
+
+    private void Start()
+    {
+        trackedImageManager.trackedImagesChanged += OnTrackedImageChanged;
+    }
+
+    private void Update()
+    {
+        if(markersPosition.Count <= 1)
+        {
+            removeButton.SetActive(false);
+        }
+        else
+        {
+            removeButton.SetActive(true);
+        }
+
+
+        if(markerObjects.Count > 0)
+        {
+            cntText.SetActive(true);
+            cntText.GetComponent<TMP_Text>().text = "Save Points : " + markerObjects.Count;
+        }
+
+
+    }
+    private void OnTrackedImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        List<ARTrackedImage> addedImages = args.added;
+        List<ARTrackedImage> removedImages = args.removed;
+
+        foreach(ARTrackedImage image in addedImages)
+        {
+            if(image.referenceImage.name == "QR")
+            {
+                InitializePosition();
+            }
+        }
+
+        cntText.SetActive(true);
+    }
+
+    private void InitializePosition()
+    {
+        if (markersPosition.Count == 0)
+        {
+            arCam.transform.position = arSession_Origin.transform.position;
+            arCam.transform.rotation = arSession_Origin.transform.rotation;
+            makeButton.SetActive(true);
+            removeButton.SetActive(true);
+            MarkerAdd();
+        }
+    }
+
+    string debstr = null;
+
+    public void MarkerAdd()
+    {
+        markersPosition.Add(arCam.transform.position);
+        markerObjects.Add( Instantiate(markerPrefab, arCam.transform.position, Quaternion.Euler(90, 0, 0) ) );
+        debstr += "(" + markersPosition.Count + ") " + markerObjects[markersPosition.Count-1].transform.position.ToString() + "\n";
+        Debug.Log(debstr);
+    }
+
+    public void MarkerAdd_EndDrill()
+    {
+        markersPosition.Add(arCam.transform.position);
+        markerObjects.Add(Instantiate(markerPrefab, arCam.transform.position, Quaternion.Euler(90, 0, 0)));
+        debstr += "(" + markersPosition.Count + ") " + markerObjects[markersPosition.Count - 1].transform.position.ToString() + "\n";
+        Debug.Log(debstr);
+    }
+
+    public void MarkerDelete()
+    {
+        if (markersPosition.Count > 1) // 첫째 마커는 못지워!
+        {
+            markersPosition.RemoveAt(markersPosition.Count - 1);
+            Destroy(markerObjects[markerObjects.Count - 1]);
+            markerObjects.RemoveAt(markerObjects.Count - 1);
+            debstr.Remove(debstr.LastIndexOf("("));
+            Debug.Log(debstr);
+        }
+    }
+}
