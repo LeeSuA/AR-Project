534776337d38b8069c4bae5791eb94055a65ce68
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2020-08-06 06:04:57 +0900

    훈련생성Scene UI 이쁘게^^
    해야할 일 :
    AR Camera의 위치를 스크립트에서 수정할 수가 없다. ARSession이나 ARSessionOrigin의 Position/Rotation을 수정해서 해야 할 듯 하다.

---
+++ EXITraining/Assets/debug.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class debug : MonoBehaviour
+{
+
+    public GameObject arSessOr, arSession, arCam;
+
+    TMP_Text txt;
+
+    private void Start()
+    {
+        txt = this.GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        txt.text = "AR Sess.Or : " + arSessOr.transform.position.ToString() + arSessOr.transform.rotation.ToString();
+        txt.text += "\nAR Session : " + arSession.transform.position.ToString() + arSession.transform.rotation.ToString();
+        txt.text += "\nAR Camera : " + arCam.transform.position.ToString() + arCam.transform.rotation.ToString();
+    }
+}
