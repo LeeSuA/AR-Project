b0f6e8485345819c8e177fc34be9f38544677cc1
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2020-08-09 03:01:37 +0900

    뺄빼넣넣

--- EXITraining/Assets/debug.cs
+++ EXITraining/Assets/debug.cs
@@ -19,10 +19,6 @@ public class debug : MonoBehaviour
     {
         txt.text = "AR Sess.Or : " + arSessOr.transform.position.ToString() + arSessOr.transform.rotation.ToString();
         txt.text += "\nAR Session : " + arSession.transform.position.ToString() + arSession.transform.rotation.ToString();
-<<<<<<< HEAD
-        txt.text += "\nAR Camera : " + arCam.transform.position.ToString() + arCam.transform.rotation.ToString();
-=======
         txt.text += "\nAR Camera : " + arCam.transform.position.ToString() + arCam.transform.rotation.ToString() + arCam.transform.rotation.eulerAngles.ToString() ;
->>>>>>> a255a2aec8bfb25472af9e768ecf2e25ed769108
     }
 }
