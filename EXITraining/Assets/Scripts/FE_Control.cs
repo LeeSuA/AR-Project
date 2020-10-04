5463b1457e4e44e2835f77e32f54fb359795e62a
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2020-10-05 05:58:23 +0900

    Extinguisher & URP
    소화기 조작을 다 만들었다.
    URP(Universal Render Pipeline) 적용하기 위해서 AR Foundation 및 AR Core 3.1.5로 업그레이드했다. 이 참에 버전 4로 업그레이드하자. 혹시라도 AR 트래킹 성능이 좋아질지도 모른다.
    +모든 3D Material에 URP 적용하고 Canvas Scaler 적용하고 AR 버전 업그레이드 해서 마스터브랜치에 병합하고 푸시하자. 다른 사람들 모두 이 변경사항 적용할 수 있도록 한다.

---
+++ EXITraining/Assets/Scripts/FE_Control.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FE_Control : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndDragHandler, IPointerUpHandler
+{
+
+    Vector3 LastTouchPoint;
+    Vector3 NowTouchPoint;
+    public Camera cam;
+    public Transform pin;
+    public Transform body;
+    public Transform handle;
+    public ParticleSystem Extinguisher_Particle;
+
+    RaycastHit firstHit;
+
+    [SerializeField]
+    bool handle_is_touched = false;
+    bool somethingHit = false;
+    bool pinOut = false;
+    
+    /* ------------------------------------- */
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnPointerDown(eventData.position);
+        HandleUp();
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (somethingHit) OnDrag(eventData.position);
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnEndDrag();
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        HandleDown();
+    }
+    /* ------------------------------------- */
+
+
+    private void OnPointerDown(Vector2 a_FirstPoint)
+    {
+        Ray touchray = cam.ScreenPointToRay(a_FirstPoint);
+        Physics.Raycast(touchray, out firstHit);
+
+        if (firstHit.collider != null)
+        {
+            somethingHit = true;
+        }
+        else somethingHit = false;
+        NowTouchPoint = a_FirstPoint;
+        LastTouchPoint = a_FirstPoint;
+
+    }
+
+    private void OnDrag(Vector2 a_SecondPoint)
+    {
+        NowTouchPoint = a_SecondPoint;
+        float deltaX = (LastTouchPoint.x - NowTouchPoint.x) / Screen.width;
+
+        if (somethingHit)
+        {
+            // 소화기 회전
+            if (firstHit.transform.tag == "Body")
+            {
+                body.transform.parent.transform.Rotate(Vector3.up * deltaX * 30f, Space.Self);
+            }
+            // 핀 뽑기
+            else if (!pinOut && firstHit.transform.tag == "Pin")
+            {
+                // if (pin.transform.position.x >= 0 && deltaX < 0) deltaX = 0; // 위치제한
+
+                if (body.transform.parent.transform.rotation.y < 90 && body.transform.parent.transform.rotation.y > -90)
+                {
+                    if (pin.transform.position.x - deltaX > 0) pin.transform.Translate(Vector3.left * pin.transform.position.x * 0.4f);
+                    else pin.transform.Translate(Vector3.left * deltaX, Space.Self);
+                }
+                else
+                {
+                    if (pin.transform.position.x + deltaX > 0) pin.transform.Translate(Vector3.right * pin.transform.position.x / 1.2f);
+                    else pin.transform.Translate(Vector3.left * deltaX, Space.Self);
+                }
+            }
+        }
+        LastTouchPoint = a_SecondPoint;
+    }
+
+    private void OnEndDrag()
+    {
+        if (!pinOut)
+        {
+            if (pin.transform.localPosition.x <= -0.3f) // 핀이 좀 나왔으면 핀 제거
+            {
+                pinOut = true;
+                handle.GetComponent<GlowEffect>().active = true;
+                StartCoroutine(PinOut());
+            }
+        }
+    }
+
+    private void HandleUp() // 핸들 누르기
+    {
+        // 핸들 누르는 동작
+        if (pinOut && somethingHit && firstHit.transform.tag == "Handle")
+        {
+            handle_is_touched = true;
+            StartCoroutine(HandleRotate_Upward());
+            Extinguisher_Particle.Play();
+        }
+    }
+
+    private void HandleDown() // 핸들 떼기
+    {
+        if (pinOut && somethingHit && firstHit.transform.tag == "Handle")
+        {
+            handle_is_touched = false;
+            StartCoroutine(HandleRotate_Downward());
+            Extinguisher_Particle.Stop();
+        }
+        somethingHit = false;
+    }
+
+    IEnumerator PinOut()
+    {
+        Material mat = pin.GetComponent<MeshRenderer>().material;
+        Vector3 pin_firstPosition = pin.transform.position;
+        while (mat.color.a >= 0.0001f)
+        {
+            mat.color = (new Color(0, 0, 0, mat.color.a - Time.deltaTime * 2));
+            pin.position = Vector3.Lerp(pin.transform.position, pin_firstPosition - Vector3.right * 0.2f, 0.1f);
+            yield return null;
+        }
+        if(mat.color.a <= 0.0001f)
+        Destroy(pin.gameObject);
+    }
+
+    IEnumerator HandleRotate_Upward()
+    {
+        Transform handlePivot = handle.transform.parent.transform;
+        while (handle_is_touched && handlePivot.localRotation.x < 0.15f)
+        {
+            handlePivot.Rotate(Time.deltaTime * 200, 0, 0);
+            yield return null;
+        }
+    }
+
+    IEnumerator HandleRotate_Downward()
+    {
+        Transform handlePivot = handle.transform.parent.transform;
+        while (!handle_is_touched && handlePivot.localRotation.x > 0)
+        {
+            handlePivot.Rotate(-Time.deltaTime * 200, 0, 0);
+            yield return null;
+        }
+    }
+}
---
+++ EXITraining/Assets/Scripts/GlowEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowEffect : MonoBehaviour
+{
+
+    Material mat;
+    float i = 0;
+    public bool active = false;
+
+    private void Start()
+    {
+        mat = this.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            if (i < 1) { i += Time.deltaTime * 0.1f; }
+            else { i = i - 1; }
+            Color color = Color.HSVToRGB(i, 1f, 1f);
+            mat.SetColor("_EmissionColor", new Vector4(color.r, color.g, color.b, 1f) * 1f);
+        }
+    }
+}
